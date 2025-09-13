8a39eaca2f6725a1991894e5faddeb5d9e8acdba
KristianaTrupja <trupja.kristiana@yahoo.com>
2025-09-13 15:44:02 +0300

    add Student entity

--- src/main/java/university/entity/Student.java
+++ src/main/java/university/entity/Student.java
@@ -1,4 +1,23 @@
 package university.entity;
+import jakarta.persistence.*;
+import java.util.*;
 
-public class Student {
+    @Entity
+    @Table(name = "students")
+    public class Student {
+        @Id
+        @GeneratedValue(strategy = GenerationType.IDENTITY)
+        private Long id;
+
+        private String name;
+
+        @Column(unique = true, nullable = false)
+        private String email;
+
+        @Column(name = "enrollment_year")
+        private int enrollmentYear;
+
+        @OneToMany(mappedBy = "student", cascade = CascadeType.ALL)
+        private List<Enrollment> enrollments = new ArrayList<>();
+
 }
